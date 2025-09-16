ba9e37de19fa43ec5bea7d24a4671ae107b81666
Dorukan <catakd@mef.edu.tr>
2025-09-16 14:23:05 +0300

    update sol reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,11 +1,23 @@
+// const reverseString = function(str) {
+    
+//     let splitStr = str.split(''); // split the string into characters
+//     let reversedStr = ''; // init empty string
+
+//     // loop over characters in reverse then add to empty string
+//     for(let i = splitStr.length - 1; i >= 0; i--){
+//         reversedStr += splitStr[i];
+//     }
+//     return reversedStr;
+// };
+
+
 const reverseString = function(str) {
     
-    let splitStr = str.split(''); // split the string into characters
     let reversedStr = ''; // init empty string
 
     // loop over characters in reverse then add to empty string
-    for(let i = splitStr.length - 1; i >= 0; i--){
-        reversedStr += splitStr[i];
+    for(let i = str.length - 1; i >= 0; i--){
+        reversedStr += str[i];
     }
     return reversedStr;
 };
