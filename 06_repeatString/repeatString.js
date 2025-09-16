fb2f46f006f4a3406b4f9d3b70946e0fb4b5c6ea
Dorukan <catakd@mef.edu.tr>
2025-09-16 12:45:57 +0300

    add 06-repeatString sol

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let totalStr = ''; // init empty string
+
+    if(num < 0){
+       return 'ERROR'; 
+    }else{
+        while(num != 0){
+        totalStr += `${string}`; 
+        num--;
+        }
+        return totalStr;
+    }
+    
 };
 
 // Do not edit below this line
