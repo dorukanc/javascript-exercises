7dd93ee73277e3e692680c483642deb45b0bd5e1
Dorukan <catakd@mef.edu.tr>
2025-09-16 13:05:05 +0300

    add sol 07-reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    
+    let splitStr = str.split(''); // split the string into characters
+    let reversedStr = ''; // init empty string
 
+    // loop over characters in reverse then add to empty string
+    for(let i = splitStr.length - 1; i >= 0; i--){
+        reversedStr += splitStr[i];
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
