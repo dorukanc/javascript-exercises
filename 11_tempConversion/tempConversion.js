f111420683a139ce2111bb8ab8502b399c4873cb
Dorukan <catakd@mef.edu.tr>
2025-09-17 14:01:21 +0300

    add sol tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    const celc = Math.round((temp - 32) * 5/9 * 10) / 10;
+    return celc;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fahrenheit = Math.round((temp * 9/5 * 10)) / 10 + 32;
+  return fahrenheit;
 };
 
 // Do not edit below this line
