73247deaa1af240f29cdbd5169319504504040be
Dorukan <catakd@mef.edu.tr>
2025-09-29 15:38:07 +0300

    add sol find the oldest person

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const date = new Date(); // get the recent date for missing ones
+    const ages = arr.map(person => {
+        if(typeof(person.yearOfDeath) !== 'number'){
+            return ({ 
+                name: person.name,
+                age: date.getFullYear() - person.yearOfBirth
+            });
+        }else{
+            return ({ 
+                name: person.name,
+                age: person.yearOfDeath - person.yearOfBirth});
+        }
+    });
 
+    // sort the ages from descending order
+    const oldestToYoungest = ages.sort((a,b) => b.age-a.age);
+    return oldestToYoungest[0]; // return the oldest person
+    
 };
 
 // Do not edit below this line
