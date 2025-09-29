aa021dca05896f4e3850b2f7887c617720524a5c
Dorukan <catakd@mef.edu.tr>
2025-09-29 13:20:31 +0300

    add 12-calculator exercise sol

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a , b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((prev, value) => prev += value, 0) || 0;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prev, value) => prev * value);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1;
+  while(a > 0){
+    factorial *= a;
+    a--;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
