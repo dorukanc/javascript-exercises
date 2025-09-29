abc7378075f53ff572f037f44eee7048a1d0e429
Dorukan <catakd@mef.edu.tr>
2025-09-29 14:53:29 +0300

    add fibonacci sol

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    // takes the last 2 values and sum them up
 
+    // 0-1-1-2-3-5-8 goes like this
+    
+    if(number < 0){
+       return 'OOPS';
+    }else{
+        let arr = [0, 1];
+        for(let i=2; i<=number; i++){
+            arr[i] = arr[i-1] + arr[i-2]; 
+        }
+        return arr[number]; 
+    }
+    
 };
 
 // Do not edit below this line
